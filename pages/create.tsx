74e0888b4903aabae6c0bab7773baa923bcc9776
Salief Lewis <salieflewis@gmail.com>
2022-08-09 13:18:22 -0400

    toast styling

--- pages/create.tsx
+++ pages/create.tsx
@@ -9,7 +9,6 @@ import {
 import toast, { Toaster } from 'react-hot-toast';
 import { useState, useEffect } from 'react';
 import router from 'next/router';
-
 import ClubFactory from '../abi/ClubFactory.json';
 
 const Create: NextPage = () => {
@@ -19,29 +18,35 @@ const Create: NextPage = () => {
 
   const [club, setClub] = useState<Club>({ name: '' });
 
-  const [clubAdd, setClubAdd] = useState<
+  const [clubAddress, setClubAddress] = useState<
     string | undefined
   >(undefined);
-  const [hash, setHash] = useState<string | undefined>(
-    undefined
-  );
-  const { status } = useWaitForTransaction({
-    hash: hash,
-  });
 
-  useEffect(() => {
-    // Update the document title using the browser API
-    console.log('STAT', status, clubAdd);
-    if (status === 'success') {
-      router.push({
-        pathname: 'populate',
-        query: {
-          clubAddress: clubAdd,
-        },
-      });
-    }
-  }, [status]);
-  // const [clubName, setClubName] = useState('');
+  const [clubName, setClubName] = useState<
+    string | undefined
+  >(undefined);
+
+  // const [hash, setHash] = useState<string | undefined>(
+  //   undefined
+  // );
+
+  // const { status } = useWaitForTransaction({
+  //   hash: hash,
+  // });
+
+  // useEffect(() => {
+  //   console.log('Status', status, clubAdd);
+  //   if (status === 'success') {
+  //     router.push({
+  //       pathname: 'populate',
+  //       query: {
+  //         clubAddress: clubAdd,
+  //       },
+  //     });
+  //   }
+  // }, [status]);
+
+  const [loading, setLoading] = useState(false);
 
   const { data, isError, isLoading, write } =
     useContractWrite({
@@ -50,17 +55,13 @@ const Create: NextPage = () => {
       contractInterface: ClubFactory,
       functionName: 'addClub',
       args: club.name,
-
-      onSuccess(cancelData, variables, context) {
-        console.log('Success!', cancelData);
-        setHash(cancelData.hash);
-      },
+      // onSuccess(cancelData, variables, context) {
+      //   console.log('Success!', cancelData);
+      //   setHash(cancelData.hash);
+      // },
       onError(error) {
         console.log('error', error);
       },
-      // onSuccess(cancelData, variables, context) {
-      //   console.log('Success!', cancelData);
-      // },
     });
 
   useContractEvent({
@@ -68,32 +69,22 @@ const Create: NextPage = () => {
       '0xc2D115ed2Eb75aEB8d598e3149a60fA035a020Fc',
     contractInterface: ClubFactory,
     eventName: 'ClubCreated',
-    // listener: (event) => (
-    //   setClubName(event[1]), console.log(clubName)
-    // ),
     listener: (event) => {
-      console.log('RH', event);
-      setClubAdd(event[0]);
-      toast.success('Club Created Successfully', {
-        duration: 4000,
-        position: 'top-left',
+      console.log('address', event[0], 'name', event[1]);
+      setClubAddress(event[0]);
+      setClubName(event[1]);
 
-        // Custom Icon
-        icon: '👏',
-        // Change colors of success/error/loading icon
-        iconTheme: {
-          primary: '#0a0',
-          secondary: '#fff',
-        },
-        // styling
+      // toast config
+      toast.success('Your club was created successfully', {
+        icon: null,
+        duration: 5000,
+        position: 'top-left',
         style: {
-          border: '1px solid #FFFDF8',
+          border: '1px solid #FFB5A7',
           padding: '8px 12px',
-          color: '#FFFDF8',
-          backgroundColor: '#1E1E1E',
-          // minWidth: '300px'
+          color: '#8F8E94',
+          backgroundColor: '#171718',
         },
-        // Aria
         ariaProps: {
           role: 'status',
           'aria-live': 'polite',
@@ -132,31 +123,6 @@ const Create: NextPage = () => {
             }}
           />
         </div>
-
-        {/* <div className='w-full font-satoshi-med mt-16'>
-          <label className='my-1 text-main-gray text-base'>
-            Add curators
-          </label>
-          <p className='text-main-gray-dark text-sm'>
-            Add wallet addresses for each collaborator in
-            this club. You will also be able to do this
-            later.
-          </p>
-          <input
-            type='text'
-            className='w-full bg-main-black border-0 border-b-2 border-cta text-main-gray-dark px-0 mt-6'
-            value={club.curators}
-            onChange={(e) => {
-              e.preventDefault();
-              setClub((current) => {
-                return {
-                  ...current,
-                  curators: [e.target.value],
-                };
-              });
-            }}
-          />
-        </div> */}
 
         <button
           className='text-lg text-main-black mt-20 bg-cta font-tr px-2 py-1 hover:bg-main-gray'
